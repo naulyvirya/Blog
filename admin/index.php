50369166094bac01aee7d6282acd946328dc6907
naulyvirya <naulyhp16@gmail.com>
2019-11-25 11:35:27 +0700

    Belajar Blog

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<!-- Header -->
+<?php include('../layouts/includes/head.php'); ?>
+<!-- End Header -->
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <!-- Navbar  -->
+    <?php include('../layouts/includes/navbar.php'); ?>
+    <!-- End Navbar -->
+
+    <div class="app-body">
+      <!-- Sidebar -->
+      <?php include('../layouts/includes/sidebar.php'); ?>
+      <!-- End Sidebar -->
+
+      <!-- Main Content -->
+      <main class="main">
+
+      </main>
+      <!-- End Main Content -->
+    </div>
+    
+    <?php include('../layouts/includes/footer.php'); ?>
+    <!-- CoreUI and necessary plugins-->
+    <!-- Scripts -->
+    <?php include('../layouts/includes/scripts.php'); ?>
+    <!-- End Scripts -->
+  </body>
+</html>
